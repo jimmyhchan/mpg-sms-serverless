9267d9952611c2e0ad5d4b84afd9f160961d6875
Jimmy Chan <jchan@linkedin.com>
2018-09-02 06:21:02 -0700

    sms example sends locally

--- handler.js
+++ handler.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+
+const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
+
+twilioClient.messages.create({
+  body: 'yeeeeeeeeeeeehaw',
+  from: process.env.TWILIO_PHONE_NUMBER,
+  to: process.env.MY_PHONE_NUMBER
+}).then(console.log.bind(console)).done();
+
+/*
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
@@ -12,3 +24,5 @@ module.exports.hello = async (event, context) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+*/
