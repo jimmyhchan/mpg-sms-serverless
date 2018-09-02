434116d8396ec65803623de3acf66e2f2d6feae5
Jimmy Chan <jchan@linkedin.com>
2018-09-02 13:58:53 -0700

    move to lambda

--- handler.js
+++ handler.js
@@ -1,28 +1,38 @@
 'use strict';
 
-const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+// const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+// const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+// const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
+const {MessagingResponse} = require('twilio').twiml;
 
-const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
-
+/*
 twilioClient.messages.create({
   body: 'yeeeeeeeeeeeehaw',
   from: process.env.TWILIO_PHONE_NUMBER,
   to: process.env.MY_PHONE_NUMBER
 }).then(console.log.bind(console)).done();
+*/
+const makeResponse = () => {
+  const twiml = new MessagingResponse();
+  twiml.message('from my lambda');
 
-/*
-module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+    headers: {
+      'Content-Type': 'text/xml',
+      isBase64Encoded: false
+      // 'access-control-allow-origin': '*',
+      // 'connection': 'close',
+    },
+    body: twiml.toString()
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  };
+};
+const smsGet = (event, context, callback) => {
+  const {headers} = event;
+  callback(null, makeResponse(headers));
 };
 
-*/
+module.exports = {
+  sms: smsGet
+};
