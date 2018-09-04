5b34e19a25b80a47ad4ead0ea2142563ba41437c
Jimmy Chan <jchan@linkedin.com>
2018-09-04 05:20:01 -0700

    start endpoint testing

--- fuelup/__tests__/fuelup-test.js
+++ fuelup/__tests__/fuelup-test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
-import {calculateMpg, parseIncomingSms} from '../fuelup-app';
+import request from 'supertest';
+import {app, calculateMpg, parseIncomingSms} from '../fuelup-app';
 
 test('calculations for mpg', t => {
   t.is(calculateMpg(5, 100, 200), '20.0 mpg');
@@ -7,4 +8,16 @@ test('calculations for mpg', t => {
 });
 test('parse incoming sms', t => {
   t.deepEqual(parseIncomingSms('123 456 789'), [123, 456, 789]);
+});
+test('endPoint testing', async t => {
+  t.plan(1);
+  const resGet = await request(app).get('/fuelup');
+  // const resPost = await request(app).post('/fuelup').send({
+  //   Body: '123 456 789'
+  // });
+  t.is(resGet.status, 200);
+  // t.is(resGet.body, 'hello from server');
+
+  // t.is(resPost.status, 200);
+  // t.is(resPost.body, 'blah');
 });
